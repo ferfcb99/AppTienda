7ecdbfd3e970304db74db72335345818c16f8b7a
Fernando Munguia <fer_fcb99@outlook.com>
2021-08-09 09:29:01 -0500

    Aplicacion tienda al 90

--- src/pedido/Paso2.java
+++ src/pedido/Paso2.java
@@ -2,22 +2,30 @@ package pedido;
 
 import java.sql.*;
 import conexion.Database;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 public class Paso2 extends javax.swing.JFrame {
     
    
     private String fecha;
     private String hora;
-    private int cantidadArticulos;
+    private int cantidadArticulos = 0;
     private double costo;
-    private int iva = 15;
-    private int descuento;
+    private int iva = 1;
+    private int descuento = 0;
     private double costoTotal;
+    
     public int codCliente;
+    // public Date date;
     // arreglo que almacena temporalmente los datos del producto
     Object[] datosProducto = new Object[3]; // 0 1 2
     // lista que guarda los articulos en ese momento
@@ -41,7 +49,38 @@ public class Paso2 extends javax.swing.JFrame {
         this.codCliente = codCliente;
     }
     
+    public String getFecha(){
+        return this.fecha;
+    }
     
+    public String getHora(){
+        return this.hora;
+    }
+    
+    
+    public int getCantidadArticulos(){
+        return this.cantidadArticulos;
+    }
+    
+     public int getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(int descuento) {
+        this.descuento = descuento;
+    }
+    
+    public double getCosto(){
+        return this.costo;
+    }
+    
+    public int getIva(){
+        return this.iva;
+    }
+    
+    public double getCostoTotal(){
+        return this.costoTotal;
+    }
     // funcion que setea la variable codCliente con el ultimo codCliente
     public void setMaxCodCliente(){
         Database db = new Database();
@@ -108,8 +147,69 @@ public class Paso2 extends javax.swing.JFrame {
     // funcion que captura los codigos
     public void capturaCodigos(){
         for(int i = 0; i<tabla_pedido.getRowCount(); i++){
-            addCodigo(String.valueOf(tabla_pedido.getValueAt(i, 0)));
+            addCodigo(String.valueOf(tabla_pedido.getValueAt(i, 0)));            
         }
+    }
+    
+    
+    public void setCantidadArticulos(){
+        this.cantidadArticulos = 0;
+        for(int i = 0; i<tabla_pedido.getRowCount(); i++){            
+            this.cantidadArticulos = this.cantidadArticulos + 1;
+        }
+    }
+    
+    
+    
+    // funcion que setea La fecha
+    public void setFecha(){
+        LocalDateTime objDate = LocalDateTime.now(); // 2021-08-09T08:14:53:123
+        // yyyy-MM-dd
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        this.fecha = String.valueOf(objDate.format(dtf));
+    }
+    
+    public void setHora(){
+        LocalDateTime objHour = LocalDateTime.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+        this.hora = String.valueOf(objHour.format(dtf));
+    }
+    
+    /*
+    public void setCostos(){
+        BigDecimal suma = BigDecimal.ZERO;
+        for(int i = 0; i<tabla_pedido.getRowCount(); i++){
+            BigDecimal costoActual = (BigDecimal)tabla_pedido.getValueAt(i, 2);
+            suma.add(costoActual);            
+        }
+        this.costo = Double.parseDouble(suma.toString());
+        setEtiquetas();
+    }*/
+    
+    
+    
+    public void setCostos(){
+        double suma = 0;
+        DecimalFormat df = new DecimalFormat("#.00");
+        for(int i = 0; i<tabla_pedido.getRowCount(); i++){
+            suma = suma + Double.parseDouble(String.valueOf(tabla_pedido.getValueAt(i, 2)));
+        }
+        String sumaParcial = df.format(suma);
+        this.costo = Double.parseDouble(sumaParcial);
+        // colocando el costo total
+        // costo + (costo * (iva / 100)) -> 0.15
+        double iva = this.iva / 100;
+        this.costoTotal = this.costo + iva*this.costo;       
+        setEtiquetas();
+        
+        
+    }
+    
+    
+    public void setEtiquetas(){
+        label_precio.setText(String.valueOf(getCosto()));
+        label_iva.setText(String.valueOf(getIva()));  
+        label_costoTotal.setText(String.valueOf(getCostoTotal()));
     }
     
    
@@ -118,24 +218,30 @@ public class Paso2 extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
+        btn_nuevoPedido = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         cb_articulos = new javax.swing.JComboBox<>();
         btn_agregar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         tabla_pedido = new javax.swing.JTable();
-        jButton4 = new javax.swing.JButton();
+        btn_confirmar = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        label_precio = new javax.swing.JLabel();
+        label_iva = new javax.swing.JLabel();
+        label_costoTotal = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setBackground(new java.awt.Color(73, 133, 172));
 
-        jButton1.setText("Registrar pedido");
-        jButton1.setBorder(null);
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btn_nuevoPedido.setText("Registrar pedido");
+        btn_nuevoPedido.setBorder(null);
+        btn_nuevoPedido.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btn_nuevoPedidoActionPerformed(evt);
             }
         });
 
@@ -176,12 +282,27 @@ public class Paso2 extends javax.swing.JFrame {
             tabla_pedido.getColumnModel().getColumn(2).setResizable(false);
         }
 
-        jButton4.setText("Confirmar pedido");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
+        btn_confirmar.setText("Confirmar pedido");
+        btn_confirmar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
+                btn_confirmarActionPerformed(evt);
             }
         });
+
+        jLabel1.setFont(new java.awt.Font("Gadugi", 0, 14)); // NOI18N
+        jLabel1.setText("Precio:");
+
+        jLabel2.setFont(new java.awt.Font("Gadugi", 0, 14)); // NOI18N
+        jLabel2.setText("Iva:");
+
+        jLabel3.setFont(new java.awt.Font("Gadugi", 0, 14)); // NOI18N
+        jLabel3.setText("Costo Total: ");
+
+        label_precio.setFont(new java.awt.Font("Gadugi", 0, 14)); // NOI18N
+
+        label_iva.setFont(new java.awt.Font("Gadugi", 0, 14)); // NOI18N
+
+        label_costoTotal.setFont(new java.awt.Font("Gadugi", 0, 14)); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -192,20 +313,33 @@ public class Paso2 extends javax.swing.JFrame {
                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGap(99, 99, 99)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(cb_articulos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel3))
+                .addGap(55, 55, 55)
+                .addComponent(label_costoTotal, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap(710, Short.MAX_VALUE)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addComponent(btn_nuevoPedido, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                                .addComponent(btn_agregar, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 53, Short.MAX_VALUE)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(btn_confirmar, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addGap(28, 28, 28))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(99, 99, 99)
-                        .addComponent(cb_articulos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(84, 84, 84)
-                        .addComponent(btn_agregar, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addGap(28, 28, 28))
+                            .addComponent(label_precio, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(label_iva, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -219,9 +353,24 @@ public class Paso2 extends javax.swing.JFrame {
                         .addComponent(btn_agregar))
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 125, Short.MAX_VALUE)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(btn_confirmar, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(label_precio, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(61, 61, 61)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(label_iva, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(24, 24, 24)
+                        .addComponent(btn_nuevoPedido, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(label_costoTotal, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addGap(27, 27, 27))
         );
 
@@ -236,9 +385,30 @@ public class Paso2 extends javax.swing.JFrame {
         p1.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btn_nuevoPedidoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_nuevoPedidoActionPerformed
+        Database db = new Database();
+        CallableStatement cs;
+        String query = "{CALL sp_NuevoPedido(?, ?, ?, ?, ?, ?, ?, ?)}";
         
-    }//GEN-LAST:event_jButton1ActionPerformed
+        try {
+            cs = db.con.prepareCall(query);
+            cs.setString(1, getFecha());
+            cs.setString(2, getHora());
+            cs.setInt(3, getCantidadArticulos());
+            cs.setDouble(4, getCosto());
+            cs.setInt(5, getIva());
+            cs.setInt(6, getDescuento());
+            cs.setDouble(7, getCostoTotal());
+            cs.setInt(8, getCodCliente());
+            
+            cs.execute();
+            JOptionPane.showMessageDialog(null, "Pedido registrado");
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al registrar pedido");
+            e.printStackTrace();
+        }
+    }//GEN-LAST:event_btn_nuevoPedidoActionPerformed
 
     private void btn_agregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_agregarActionPerformed
         // boton que agrega la informacion del articulo
@@ -247,13 +417,20 @@ public class Paso2 extends javax.swing.JFrame {
        
     }//GEN-LAST:event_btn_agregarActionPerformed
 
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        setMaxCodCliente();
+    private void btn_confirmarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_confirmarActionPerformed
         capturaCodigos();
-        for(String codigo : codigosArticulos){
-            System.out.println(codigo);
-        }
-    }//GEN-LAST:event_jButton4ActionPerformed
+        setCantidadArticulos();
+        setCostos();
+        setFecha();
+        setHora();
+        System.out.println(getCantidadArticulos());
+        System.out.println(getCosto());
+        System.out.println(getIva());
+        System.out.println(getCostoTotal());
+        System.out.println(getFecha());
+        System.out.println(getHora());
+        System.out.println(getDescuento());
+    }//GEN-LAST:event_btn_confirmarActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -289,12 +466,18 @@ public class Paso2 extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btn_agregar;
+    private javax.swing.JButton btn_confirmar;
+    private javax.swing.JButton btn_nuevoPedido;
     private javax.swing.JComboBox<String> cb_articulos;
-    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton4;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel label_costoTotal;
+    private javax.swing.JLabel label_iva;
+    private javax.swing.JLabel label_precio;
     private javax.swing.JTable tabla_pedido;
     // End of variables declaration//GEN-END:variables
 }
